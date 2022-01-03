e89a2462b57abac32293eba47aee4e399b622646
ElvisDev187 <84122281+ElvisDev187@users.noreply.github.com>
2022-01-03 08:02:09 +0100

    update due au modification de ifExist et Validators

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+//connection à la base de données
+mongoose.connect(process.env.DB_TEST,{})
+.then(function (){
+    console.log("connexion à la base de donnée établie")
+})
+.catch(e=>{
+    console.log("erreur de connexion :"+e )
+});
 
+const workerRoute = require('./routes/worker')
 
 
 
@@ -23,6 +32,8 @@ app.use(cors());
 app.use(helmet());
 // Import des routes
  require('./routes/PosteRout')(app)
+ app.use('/api/worker', workerRoute)
+
 
 
 
@@ -30,12 +41,6 @@ app.use(helmet());
 
 
 app.get('/', (req, res)=>{ res.status(200).json({message : "Bienvenu sur l'api UADGest"}) })
-
-
-mongoose.connect(process.env.DB_TEST, ()=> console.log("connect to bd"))
-
-
-
 
 //lancement
 app.listen(3000, ()=> console.log("server Start"));
