ec4ceb539153e5003c251b70d6d9acd0cb14b7d7
ElvisDev187 <84122281+ElvisDev187@users.noreply.github.com>
2022-01-03 08:27:00 +0100

    usage de router

--- routes/PosteRout.js
+++ routes/PosteRout.js
@@ -1,99 +1,26 @@
 
+const express = require('express');
 const controller = require('../controllers/PosteController');
-const { ifExist } = require('../middlewares/Exist');
+
+const router = express.Router();
 
 
 
-module.exports = function(app) {
-    //creer un poste je dois ajouter if exist ici
-    app.post('poste/register',[ifExist], controller.create 
-    );
+//creer un poste je dois ajouter if exist ici
+    
+router.post('/register', controller.create );
+
+router.get('/:postName', controller.getByName);
+
+router.get('/:postId', controller.getById);
 
 
-
-app.get('/poste/:postName', controller.getByName);
-
-app.get('/poste/:postId', controller.getById);
+router.delete('/:postId', controller.delete);
 
 
-app.delete('/poste/:postId', controller.delete);
+router.patch('/:postId', controller.update )
 
+router.get('/', controller.getAll);
 
-app.patch('/poste/:postId',[ifExist], controller.update )
+module.exports = router;
 
-app.get('/poste', controller.getAll);
-}
-
-
-// // lister les postes
-
-// router.get('/', async(req, res)=>{
-//     try {
-//         const postes = await Poste.find();
-//         res.status(200).send(postes);
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// });
-
-// // chercher un post en particulier par son nom
-// router.get('/:postName', async(req, res)=>{
-//     try {
-//         const poste = await Poste.findOne({nom_poste: req.params.postName});
-//         res.status(200).send(poste);
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// });
-
-
-// // chercher un post en particulier par son Id
-// router.get('/:postId', async(req, res)=>{
-//     try {
-//         const poste = await Poste.findOne({id_poste: req.params.postId});
-//         res.status(200).send(poste);
-//     } catch (error) {
-//         res.status(400).send(error);
-//     }
-// });
-
-
-
-
-// // supprimer un poste specifique
-// router.delete('/:postId', async(req, res)=>{
-
-//     try {
-//         const Removepost = await Poste.remove({ id_poste: req.params.postId})
-//         res.status(200).send(Removepost)
-//     } catch (error) {
-//        res.status(400).send({message: error})
-//     }
-   
-//    })
-
-//    // update un poste
-// router.patch('/:postId', async(req, res)=>{
-
-//     // validation
-//     const {error} =  posteValidation(req.body);
-//     if(error) return res.status(400).send(error.details[0].message);
-
-//     const poste = new Poste({
-//         id_poste: req.body.id,
-//         nom_poste: req.body.name,
-//         salaire: req.body.salaire,
-//         grade: req.body.grade
-//     });
-
-//     try {
-//         const Updateposte = await Poste.updateOne({ id_poste: req.params.postId}, 
-//             {$set: poste})
-//         res.status(200).send(Updateposte)
-//     } catch (error) {
-//        res.status(400).send({message: error})
-//     }
-   
-//    });
-
-// module.exports = router
